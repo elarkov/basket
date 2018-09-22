df793885d9deb57f89d8ce36babbb1e1b9cc924a
Evgeny Larkov <elarkov@inbox.ru>
2018-09-22 23:14:54 +0700

    fix gulp.js and add descriptipn readme.md

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,10 @@ var gulp          = require('gulp'),
 		rename        = require('gulp-rename'),
 		autoprefixer  = require('gulp-autoprefixer'),
 		notify        = require("gulp-notify"),
+		del           = require("del"),
+		imagemin      = require('gulp-imagemin'), 
+		pngquant      = require('imagemin-pngquant'),
+		cache         = require('gulp-cache'),
 		rsync         = require('gulp-rsync');
 
 gulp.task('browser-sync', function() {
@@ -60,10 +64,45 @@ gulp.task('rsync', function() {
 	}))
 });
 
+
+
 gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
 	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
 	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
 	gulp.watch('app/*.html', browserSync.reload)
+});
+
+gulp.task('clean', function(){
+	return del.sync('dist');
+});
+
+gulp.task('img', function() {
+	return gulp.src('app/img/**/*') // Берем все изображения из app
+		.pipe(cache(imagemin({ // С кешированием
+		// .pipe(imagemin({ // Сжимаем изображения без кеширования
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))/**/)
+		.pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+});
+
+
+gulp.task('build', ['clean', 'img', 'styles', 'js'], function(){
+	var buildCss = gulp.src([
+		'app/css/main.min.css',
+	])
+	.pipe(gulp.dest('dist/css'));
+
+	var buildFonts = gulp.src('app/fonts/**/*')
+		.pipe(gulp.dest('dist/fonts'));
+
+	var buildJs = gulp.src('app/js/**/*')
+		.pipe(gulp.dest('dist/js'));
+
+	var builsHtml = gulp.src('app/*.html')
+		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['watch']);
---
+++ readme.md
@@ -0,0 +1,1 @@
+**Посмотреть демо:** http://elarkov.ru/basket/
